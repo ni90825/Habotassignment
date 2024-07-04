90d7ba0b0020d7c489e623cebb7fa8b8badf9891
Nitesh bovi <2020.nitesh.bovi@ves.ac.in>
2024-07-04 16:30:33 +0530

    header resposiveness added

--- src/Components/header.js
+++ src/Components/header.js
@@ -3,14 +3,14 @@ import darklogo from "../Assets/darklogo.svg"
 
 export default function Header() {
   return (
-   <header className='flex justify-between mx-10 my-4 pt-4'>
+   <header className='flex justify-between mx-10 my-4 pt-1 items-center'>
    <div>
-    <img src={darklogo} alt='logo'></img>
+    <img src={darklogo} alt='logo' width={"140px"} height={"100px"}></img>
    </div>
    <div className='flex gap-7 items-center'>
-    <div className='font-Poppins font-normal text-[#6D6E71]'>Find Suppliers</div>
-    <div className='font-Poppins font-normal text-[#6D6E71]'>Find Service Tags <span className='rotate-180'>{"<"}</span></div>
-    <div className='font-inter font-bold text-[#00732F] border-[1.7px] border-[#00732F] w-48 flex justify-center items-center h-[50px] rounded-md'>Login / Sign Up</div>
+    { window.innerWidth > "1024" && <div className='font-Poppins font-normal text-[#6D6E71]'>Find Suppliers</div>}
+   { window.innerWidth > "1024" && <div className='font-Poppins font-normal text-[#6D6E71]'>Find Service Tags</div>}
+    <div className='font-inter font-bold text-[#00732F] border-[1.7px] border-[#00732F] w-36 sm:w-48 flex justify-center items-center h-[50px] rounded-md text-sm sm:text-base'>Login / Sign Up</div>
    </div>
    </header>
   )
